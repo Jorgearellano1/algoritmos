bf1eb90ca3900b529fe1511e829a01b4c02ac0ba
Jorge Arellano <jorge.arellano@utec.edu.pe>
2023-08-20 23:50:15 -0500

    terminado

--- main.cpp
+++ main.cpp
@@ -4,8 +4,7 @@
 #include <cmath>
 #include <fstream>
 #include <chrono>
-
-
+#include <random>
 
 int getDigit(int num, int pos) {
     return (num / static_cast<int>(pow(10, pos))) % 10;
@@ -18,7 +17,7 @@ int getMax(const std::vector<int>& arr) {
 void countingSortByDigit(std::vector<int>& arr, int pos) {
     int n = arr.size();
     std::vector<int> output(n);
-    std::vector<int> count(10, 0); // 10 dígitos posibles (0-9)
+    std::vector<int> count(10, 0);
 
     for (int i = 0; i < n; i++) {
         count[getDigit(arr[i], pos)]++;
@@ -55,52 +54,70 @@ int* Random_Array(int size) {
     return array;
 }
 
-void CountingSort(int* &array, int size) {
-    int* temp_cont = new int[10];
-    int* sorted = new int[size];
+template <typename T>
+void counting_sort(T *arr, int n) {
+    if (n == 0)
+        return;
 
-    for (int i = 0; i < 10; i++) {
-        temp_cont[i] = 0;
-        for (int j = 0; j < size; j++) {
-            if (i == array[j]) {
-                temp_cont[i]++;
-            }
+    T max_val = arr[0];
+    for (int i = 1; i < n; ++i) {
+        if (arr[i] > max_val) {
+            max_val = arr[i];
         }
     }
 
-    for (int i = 1; i < 10; i++) {
-        temp_cont[i] += temp_cont[i-1];
+    int *count = new int[max_val + 1];
+    for (int i = 0; i <= max_val; ++i) {
+        count[i] = 0;
     }
 
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < 10; j++) {
-            if (j == array[i]) {
-                sorted[temp_cont[j]-1] = array[i];
-                temp_cont[j]--;
-            }
+    for (int i = 0; i < n; ++i) {
+        ++count[arr[i]];
+    }
+
+    int index = 0;
+    for (int i = 0; i <= max_val; ++i) {
+        for (int j = 0; j < count[i]; ++j) {
+            arr[index++] = i;
         }
     }
 
-    for (int i = 0; i < size; i++) {
-        array[i] = sorted[i];
-    }
-
-    delete[] temp_cont;
-    delete[] sorted;
+    delete[] count;
 }
 
+
+template <typename T>
+void selection_sort(T* arr, int n) {
+    for (int i = 0; i < n-1; i++) {
+        int min_idx = i;
+        for (int j = i+1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+        if (min_idx != i) {
+            std::swap(arr[i], arr[min_idx]);
+        }
+    }
+}
 int main() {
     srand(time(NULL));
 
-    std::vector<int> n_values = {10, 100, 1000, 10000, 100000, 1000000, 10000000};
+    std::vector<int> n_values = {10, 100, 1000, 10000, 100000};
     std::vector<double> time_radix;
     std::vector<double> time_counting;
+    std::vector<double> time_selection;
 
     for (int n : n_values) {
         std::vector<int> arr(n);
-        int* array = Random_Array(n);
+        int* counting_array = new int[n];
+        int* selection_array = new int[n];
+
         for (int i = 0; i < n; i++) {
-            arr[i] = rand() % (n * 10);
+            int val = rand() % (n * 10);
+            arr[i] = val;
+            counting_array[i] = val;
+            selection_array[i] = val;
         }
 
         auto start_radix = std::chrono::high_resolution_clock::now();
@@ -110,27 +127,35 @@ int main() {
         time_radix.push_back(elapsed_radix.count());
 
         auto start_counting = std::chrono::high_resolution_clock::now();
-        CountingSort(array, n);
+        counting_sort(counting_array, n);
         auto end_counting = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double> elapsed_counting = end_counting - start_counting;
         time_counting.push_back(elapsed_counting.count());
 
-        delete[] array;
+        auto start_selection = std::chrono::high_resolution_clock::now();
+        selection_sort(selection_array, n);
+        auto end_selection = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> elapsed_selection = end_selection - start_selection;
+        time_selection.push_back(elapsed_selection.count());
+
+        delete[] counting_array;
+        delete[] selection_array;
     }
 
     std::ofstream dataFile("data.txt");
     for (size_t i = 0; i < n_values.size(); i++) {
-        dataFile << n_values[i] << " " << time_radix[i] << " " << time_counting[i] << std::endl;
+        dataFile << n_values[i] << " " << time_radix[i] << " " << time_counting[i] << " " << time_selection[i] << std::endl;
     }
     dataFile.close();
 
     FILE* pipe = popen("gnuplot -persist", "w");
     if (pipe) {
         fprintf(pipe, "set xlabel 'Number of Random Integers'\n");
-        fprintf(pipe, "set xlabel 'Number of Random Integers'\n");
-        fprintf(pipe, "set ylabel 'Tiempo (s)'\n");
-        fprintf(pipe, "set title 'Comparación de tiempos: Radix Sort vs Counting Sort'\n");
-        fprintf(pipe, "plot 'data.txt' using 1:2 with lines title 'Radix Sort', 'data.txt' using 1:3 with lines title 'Counting Sort'\n");
+        fprintf(pipe, "set ylabel 'Time (s)'\n");
+        fprintf(pipe, "set logscale y\n");
+
+        fprintf(pipe, "set title 'Comparison of Sorting Algorithms: Radix Sort vs Counting Sort vs Selection Sort'\n");
+        fprintf(pipe, "plot 'data.txt' using 1:2 with lines title 'Radix Sort', 'data.txt' using 1:3 with lines title 'Counting Sort', 'data.txt' using 1:4 with lines title 'Selection Sort'\n");
         fflush(pipe);
         pclose(pipe);
     }
